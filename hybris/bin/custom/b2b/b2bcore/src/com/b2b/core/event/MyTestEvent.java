f465a79da226091589a31e65dd07878b830aed93
Manohar Cheruvupalle <manohar.cheruvupalle@knacksystems.com>
2019-07-10 21:22:42 +0530

    first commit

---
+++ hybris/bin/custom/b2b/b2bcore/src/com/b2b/core/event/MyTestEvent.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package com.b2b.core.event;
+
+/**
+ * @author manoharc
+ *
+ */
+public class MyTestEvent
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args)
+	{
+		System.out.println("this is test event class");
+	}
+
+}
